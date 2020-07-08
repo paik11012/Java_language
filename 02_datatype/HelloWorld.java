f834e93dccf43e472f30035aa4e3287c665ff1f6
paik11012 <minju11012@gmail.com>
2020-07-08 17:13:56 +0900

    java first string file

---
+++ 02_datatype/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+        public static void main(String[] args) {
+            args = new String[1];
+            args[0] = "Hello World this is java";
+            System.out.println(args[0]);
+    }
+
+} 
+
+// https://opentutorials.org/course/1223
